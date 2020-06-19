b6bb28a9e9c9067adeebf2fa04e0783243cbb5f5
Elizabeth Harper <foxcapade@gmail.com>
2020-06-19 12:38:13 -0400

    sql

---
+++ src/main/resources/sql/select/studyaccess/projects/all.sql
@@ -0,0 +1,4 @@
+SELECT
+  *
+FROM
+  studyaccess.projects
---
+++ src/main/resources/sql/select/studyaccess/staff/by-id.sql
@@ -0,0 +1,22 @@
+SELECT
+  s.user_id,
+  s.is_owner,
+  a.email,
+  (
+    SELECT value
+    FROM useraccounts.account_properties
+    WHERE user_id = s.user_id
+    AND key = 'first_name'
+  ) first_name,
+  (
+    SELECT value
+    FROM useraccounts.account_properties
+    WHERE user_id = s.user_id
+    AND key = 'last_name'
+  )
+FROM
+  studyaccess.staff s
+  INNER JOIN useraccounts.accounts a
+     ON s.user_id = a.user_id
+WHERE
+  s.staff_id = ?

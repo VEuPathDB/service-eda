bf0d744a6cae9ca6c34ed2546e7c399259d680ec
Elizabeth Harper <foxcapade@gmail.com>
2020-07-20 10:55:15 -0400

    staff service logging.

--- src/main/java/org/veupathdb/service/eda/access/service/StaffService.java
+++ src/main/java/org/veupathdb/service/eda/access/service/StaffService.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Request;
 
+import org.apache.logging.log4j.Logger;
+import org.veupathdb.lib.container.jaxrs.providers.LogProvider;
 import org.veupathdb.lib.container.jaxrs.providers.UserProvider;
 import org.veupathdb.service.access.generated.model.*;
 import org.veupathdb.service.access.model.PartialStaffRow;
@@ -14,12 +16,15 @@ import org.veupathdb.service.access.util.Keys;
 
 public class StaffService
 {
+  private static final Logger log = LogProvider.logger(StaffService.class);
+
   /**
    * Looks up the current user and checks if they are a site owner.
    *
    * @return whether or not the current user is a site owner.
    */
   public static boolean userIsOwner(Request req) {
+    log.trace("StaffService#userIsOwner(req)");
     return userIsOwner(UserProvider.lookupUser(req)
       .orElseThrow(InternalServerErrorException::new)
       .getUserId());
@@ -31,6 +36,7 @@ public class StaffService
    * @return whether or not the given userId belongs to a site owner.
    */
   public static boolean userIsOwner(long userId) {
+    log.trace("StaffService#userIsOwner(userId)");
     try {
       return StaffRepo.Select.byUserId(userId)
         .filter(StaffRow::isOwner)
@@ -43,6 +49,7 @@ public class StaffService
   }
 
   public static void updateStaffRow(final StaffRow row) {
+    log.trace("StaffService#updateStaffRow(row)");
     try {
       StaffRepo.Update.ownerFlagById(row);
     } catch (Exception e) {
@@ -51,6 +58,7 @@ public class StaffService
   }
 
   public static StaffList getStaffList(int limit, int offset) {
+    log.trace("StaffService#getStaffList(limit, offset)");
     try {
       return rows2StaffList(
         StaffRepo.Select.list(limit, offset),
@@ -62,6 +70,7 @@ public class StaffService
   }
 
   public static StaffRow requireStaffById(final int staffId) {
+    log.trace("StaffService#requireStaffById(staffId)");
     try {
       return StaffRepo.Select.byId(staffId).orElseThrow(NotFoundException::new);
     } catch (Exception e) {
@@ -70,6 +79,7 @@ public class StaffService
   }
 
   public static int createStaff(final NewStaffRequest req) {
+    log.trace("StaffService#createStaff(req)");
     final var row = new PartialStaffRow();
     row.setUserId(req.getUserId());
     row.setOwner(req.getIsOwner());
@@ -82,6 +92,7 @@ public class StaffService
   }
 
   public static void deleteStaff(final int staffId) {
+    log.trace("StaffService#deleteStaff(staffId)");
     try {
       StaffRepo.Delete.byId(staffId);
     } catch (Exception e) {
@@ -90,6 +101,7 @@ public class StaffService
   }
 
   public static void validatePatch(List < StaffPatch > entity) {
+    log.trace("StaffService#validatePatch(entity)");
     if (entity.isEmpty())
       throw new BadRequestException();
 
@@ -110,6 +122,7 @@ public class StaffService
     final int offset,
     final int total
   ) {
+    log.trace("StaffService#rows2StaffList(rows, offset, total)");
     final var out = new StaffListImpl();
 
     out.setOffset(offset);
@@ -123,6 +136,7 @@ public class StaffService
   }
 
   private static Staff row2Staff(final StaffRow row) {
+    log.trace("StaffService#row2Staff(row)");
     final var user = new UserDetailsImpl();
     user.setOrganization(row.getOrganization());
     user.setFirstName(row.getFirstName());

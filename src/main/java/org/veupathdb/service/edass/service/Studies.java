cc3443f5d0eede23457e94eab7667408378b0771
steve fischer <sfischer@upenn.edu>
2020-09-09 14:42:15 -0400

    more initial iterations on API

--- src/main/java/org/veupathdb/service/edass/service/Studies.java
+++ src/main/java/org/veupathdb/service/edass/service/Studies.java
@@ -2,33 +2,35 @@ package org.veupathdb.service.edass.service;
 
 import java.util.ArrayList;
 
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Request;
-
+import org.veupathdb.service.edass.generated.model.EntityIdPostRequest;
 import org.veupathdb.service.edass.generated.model.StudiesGetResponseImpl;
 import org.veupathdb.service.edass.generated.model.StudyOverview;
 
 public class Studies implements org.veupathdb.service.edass.generated.resources.Studies {
 
-  @Context
-  private Request req;
-
- @Override
+  @Override
   public GetStudiesResponse getStudies() {
-   var out = new StudiesGetResponseImpl();
-   out.setStudies(new ArrayList<StudyOverview>());
-   return GetStudiesResponse.respond200WithApplicationJson(out);
- }
+    var out = new StudiesGetResponseImpl();
+    out.setStudies(new ArrayList<StudyOverview>());
+    return GetStudiesResponse.respond200WithApplicationJson(out);
+  }
 
   @Override
-  public GetStudiesStudiesByStudyIdResponse getStudiesStudiesByStudyId(String studyId) {
+  public GetStudiesByStudyIdResponse getStudiesByStudyId(String studyId) {
     // TODO Auto-generated method stub
     return null;
   }
 
   @Override
-  public GetStudiesStudiesStudiesByStudyIdAndEntityIdResponse getStudiesStudiesStudiesByStudyIdAndEntityId(
-      String studyId, String entityId) {
+  public GetStudiesByStudyIdAndEntityIdResponse getStudiesByStudyIdAndEntityId(String studyId,
+      String entityId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public PostStudiesByStudyIdAndEntityIdResponse postStudiesByStudyIdAndEntityId(String studyId,
+      String entityId, EntityIdPostRequest entity) {
     // TODO Auto-generated method stub
     return null;
   }

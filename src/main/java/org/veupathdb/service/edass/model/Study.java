df06a909ddef3f09e91d39a9275fa0f141c0f780
steve fischer <sfischer@upenn.edu>
2020-09-27 17:22:48 -0400

    validate entity tree IDs

--- src/main/java/org/veupathdb/service/edass/model/Study.java
+++ src/main/java/org/veupathdb/service/edass/model/Study.java
@@ -2,6 +2,7 @@ package org.veupathdb.service.edass.model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -74,6 +75,7 @@ public class Study {
    */
   void initEntitiesAndVariables(TreeNode<Entity> rootEntityNode, Set<Variable> vars) {
     entityTree = rootEntityNode;
+    validateEntityTreeIds(rootEntityNode);
     
     // build entity ID map
     entityIdMap = new HashMap<String, Entity>();
@@ -88,21 +90,6 @@ public class Study {
     variableIdToEntityMap = new HashMap<String, Entity>();
     vars.stream().map(v -> variableIdToEntityMap.put(v.getName(), entityIdMap.get(v.getEntityId())));
    }
-
-  /*
-   * Confirm that a proposed child does not conflict with existing children.
-   * Throw runtime exception if invalid
-   */
-  private static void validateChild(Entity child, TreeNode<Entity> parentNode) {
-    String errPrefix = "In entity " + parentNode.getContents().getEntityId() + ", trying to add a child with the same ID as ";
-
-    if (child.getEntityId().equals(parentNode.getContents().getEntityId()))
-      throw new InternalServerErrorException(errPrefix + "the parent: " + child.getEntityId());
-
-    if (parentNode.getChildNodes().stream()
-        .filter(ch -> ch.getContents().getEntityId() == child.getEntityId()).count() != 0) 
-      throw new InternalServerErrorException(errPrefix + "another child: " + child.getEntityId());
-  }  
 
   public Entity getEntity(String entityId) {
     return entityIdMap.get(entityId);
@@ -124,4 +111,23 @@ public class Study {
       populateEntityAncestors(childNode, ancestorEntities);
     }
   }
+  
+  /*
+   * Confirm that children have non-conflicting entity IDs. Throw runtime exception if invalid
+   */
+  private static void validateEntityTreeIds(TreeNode<Entity> entityNode) {
+    String errPrefix = "In entity " + entityNode.getContents().getEntityId() +
+        ", found a child with the same ID as ";
+
+    Set<String> childEntityIds = new HashSet<String>();
+    for (TreeNode<Entity> child : entityNode.getChildNodes()) {
+      Entity childEntity = child.getContents();
+      if (childEntity.equals(entityNode.getContents().getEntityId()))
+        throw new InternalServerErrorException(errPrefix + "the parent: " + childEntity.getEntityId());
+
+      if (childEntityIds.contains(childEntity.getEntityId()))
+        throw new InternalServerErrorException(errPrefix + "another child: " + childEntity.getEntityId());
+    }
+  }
+
 }

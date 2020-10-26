520331ac90f7a61b039e91939d1bad2fecc1f204
steve fischer <sfischer@upenn.edu>
2020-10-25 21:40:45 -0400

    add variables meta data

--- src/main/resources/org/veupathdb/service/edass/stubdb/insertDbData.sql
+++ src/main/resources/org/veupathdb/service/edass/stubdb/insertDbData.sql
@@ -21,7 +21,17 @@ insert into variableType values (200, 'string');
 insert into variableType values (300, 'number');
 insert into variableType values (400, 'date');
 
-
+--(variable_id, variable_type_id, entity_id, parent_entity_id, display_name, has_values, is_continuous, units, precision)
+insert into variable values ('var-10', 200, 'GEMS-House', null, 'roof', 1, 0, null, null);
+insert into variable values ('var-11', 300, 'GEMS-Part', null, 'shoe size', 1, 0, 'shoe size', 1);
+insert into variable values ('var-17', 200, 'GEMS-Part', null, 'hair color', 1, 0, null, null);
+insert into variable values ('var-18', 300, 'GEMS-Part', null, 'net worth', 1, 1, 'dollars', null);
+insert into variable values ('var-12', 300, 'GEMS-PartObs', null, 'weight', 1, 1, 'pounds', 2);
+insert into variable values ('var-13', 300, 'GEMS-PartObs', null, 'fav number', 1, 0, '', null);
+insert into variable values ('var-14', 400, 'GEMS-PartObs', null, 'birth date', 1, 1, 'date', null);
+insert into variable values ('var-15', 300, 'GEMS-PartObs', null, 'start date', 1, 0, null, null);
+insert into variable values ('var-16', 200, 'GEMS-PartObs', null, 'mood', 1, 0, null, null);
+insert into variable values ('var-19', 200, 'GEMS-HouseObs', null, 'water supply', 1, 0, null, null);
 
 ------------------------------------------------------------
 -- STUDY DATA TABLES

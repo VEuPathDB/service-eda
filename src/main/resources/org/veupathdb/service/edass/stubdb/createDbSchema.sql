384fc66b6e2e0ae67fb7600d15f68efe10957f10
steve-fischer-200 <sfischer@upenn.edu>
2020-12-22 14:56:11 -0500

    update schema as per JB

--- src/main/resources/org/veupathdb/service/edass/stubdb/createDbSchema.sql
+++ src/main/resources/org/veupathdb/service/edass/stubdb/createDbSchema.sql
@@ -2,60 +2,52 @@
 -- the abbrev would be used in the name of the tall and ancestors tables
 -- the study_id is a stable ID
 create table Study (
---study_id  integer,                  /* JB not needed */
-  stable_id varchar(50) not null,     /* JB previously source_id */
-  name varchar(30) not null,
+  stable_id varchar(50) not null,
+  display_name varchar(30) not null,
+  internal_abbrev varchar(30),
   PRIMARY KEY (stable_id)
 );
+alter table Study add unique (display_name);
+alter table Study add unique (internal_abbrev);
+
+create table EntityTypeName {
+  study_stable_id,
+  entity_stable_id,
+  description varchar(100),
+  display_name varchar(30) not null,
+  display_name_plural varchar(30) ,
+}
+alter table EntityTypeName add unique (study_stable_id, stable_id);
+alter table EntityTypeGraph add unique (display_name, study_stable_id, stable_id);
 
 -- The entities types per study, eg Participants, and their tree relationships
 CREATE TABLE EntityTypeGraph (
---entity_type_graph_id integer,               /* JB not needed */
-  stable_id varchar(50) not null,             /* JB previously entity_type_stable_id */
-  display_name varchar(30) not null,          /* JB previously entity_type_name */
-  display_name_plural varchar(30) not null,   /* JB need a plural column */
-  study_stable_id varchar(50) not null,       /* JB previously study_id */
-  parent_stable_id varchar(30),               /* JB previously parent_entity_type_stable_id */
-  description varchar(100),
-  abbrev varchar(20),
-  PRIMARY KEY (stable_id),
+  stable_id varchar(50) not null,
+  study_stable_id varchar(50) not null,
+  parent_stable_id varchar(30),
+  internal_abbrev varchar(20),
 );
-alter table EntityTypeGraph add unique (display_name, study_stable_id);
-alter table EntityTypeGraph add unique (parent_stable_id, study_stable_id);
-alter table EntityTypeGraph add unique (stable_id, study_stable_id);
-ALTER TABLE EntityTypeGraph 
+alter table EntityTypeGraph add unique (study_stable_id, stable_id);
+alter table EntityTypeGraph add unique (study_stable_id, internal_abbrev);
+ALTER TABLE EntityTypeGraph
    ADD FOREIGN KEY (study_stable_id) REFERENCES Study (stable_id);
 
--- The variables/categories in a study, as a tree.  (It is called a graph because in non-EDA applications
--- attributes might have multiple parents.)
-create table AttributeGraph (
---attribute_graph_id               /* JB not needed */
-  stable_id varchar(30),           /* JB previously 'source_id' */
-  study_stable_id varchar(30) not null,  /* JB previously study_id */
+--------------------------------------------------------------------------------------------------
+-- the following tables are per-study.  Their name is formed using the study ID, in this case ds2324.
+-- Each entity type gets two tables:
+--    AttrVal_XXXXX_YYYYY
+--    Ancestors_XXXXX_YYYYY
+--  where XXXXX is the study internal_abbrev and YYYYY is the entity's internal_abbrev
+--------------------------------------------------------------------------------------------------
+create table Attribute_ds2324_Hshld {
+  stable_id varchar(30),
   ontology_term_id integer,
-  parent_stable_id varchar(30),      /* JB previously 'parent_source_id' */
-  parent_ontology_term_id integer,   /* JB why do we need this? */
+  parent_stable_id varchar(30),
   provider_label varchar(30) not null,
   display_name varchar(30) not null,
   term_type varchar(20),
-  PRIMARY KEY (stable_id)
-);
-alter table AttributeGraph add unique (ontology_term_id);
-ALTER TABLE AttributeGraph
-   ADD FOREIGN KEY (study_stable_id) REFERENCES Study (stable_id);
-ALTER TABLE AttributeGraph 
-   ADD FOREIGN KEY (parent_stable_id) REFERENCES AttributeGraph (stable_id);
-
--- the attributes that have values (ie, are not strictly a category).
--- rows here link to the AttributeGraph table for positioning in the tree.
--- we allow categories to have values, so items in this table might
--- link to rows in AttributeGraph that are parents there.
-create table Attribute (
---attribute_id integer              /* JB not needed */
-  stable_id varchar(30),            /* JB previously 'source_id varchar(255)'  (why so large?) */
-  entity_type_stable_id varchar(30),
   process_type_id integer,
-  ontology_term_id integer,         /* JB this seems redundant with the same col in AttributeGraph (a join away) */
+  has_value integer,
   data_type varchar(10),
   has_multiple_values_per_entity integer,
   data_shape varchar(10),
@@ -64,92 +56,81 @@ create table Attribute (
   precision integer,
   PRIMARY KEY (stable_id)
 );
-ALTER TABLE Attribute 
-   ADD FOREIGN KEY (entity_type_stable_id) REFERENCES EntityTypeGraph (stable_id);
 
---------------------------------------------------------------------------------------------------
--- the following tables are per-study.  Their name is formed using the study ID, in this case 1000.
--- Each entity type gets two tables:
---    AttrVal_XXXXX_YYYYY
---    Ancestors_XXXXX_YYYYY
---  where XXXXX is the study ID and YYYYY is the entity's abbreviation
---------------------------------------------------------------------------------------------------
-
-create table AttrVal_1000_Hshld (
-  hshld_id integer,
-  ontology_term_id integer,
+create table AttrVal_ds2324_Hshld (
+  hshld_id varchar(20),             -- use the attribute internal_abbrev for the name of this table
+  attribute_stable_id varchar(30),         -- might be stable id
   number_value integer, 
   string_value varchar(100),
   date_value varchar(30),
 );
-ALTER TABLE AttrVal_1000_Hshld
-   ADD FOREIGN KEY (ontology_term_id) REFERENCES AttributeGraph (ontology_term_id);
-CREATE INDEX AttrVal_1000_Hshld_i1
-ON AttrVal_1000_Hshld (ontology_term_id, hshld_id, number_value, string_value, date_value);
+ALTER TABLE AttrVal_ds2324_Hshld
+   ADD FOREIGN KEY (attribute_stable_id) REFERENCES Attribute_ds2324_Hshld (stable_id);
+CREATE INDEX AttrVal_ds2324_Hshld_i1
+ON AttrVal_ds2324_Hshld (attribute_stable_id, hshld_id, number_value, string_value, date_value);
 -- for test db only
-CREATE unique INDEX AttrVal_1000_Hshld_i2 ON AttrVal_1000_Hshld (ontology_term_id, hshld_id);
+CREATE unique INDEX AttrVal_ds2324_Hshld_i2 ON AttrVal_ds2324_Hshld (attribute_stable_id, hshld_id);
 
-create table Ancestors_1000_Hshld (
+create table Ancestors_ds2324_Hshld (
   hshld_id integer,
   PRIMARY KEY (Hshld_id)
 );
 
-create table AttrVal_1000_HshldObsrvtn (
+create table AttrVal_ds2324_HshldObsrvtn (
   HshldObsrvtn_id integer,
-  ontology_term_id integer,
+  attribute_stable_id varchar(30),
   number_value integer, 
   string_value varchar(100),
   date_value varchar(30),
 );
-ALTER TABLE AttrVal_1000_HshldObsrvtn
-   ADD FOREIGN KEY (ontology_term_id) REFERENCES AttributeGraph (ontology_term_id);
-CREATE INDEX AttrVal_1000_HshldObsrvtn_i1
-ON AttrVal_1000_HshldObsrvtn (ontology_term_id, HshldObsrvtn_id, number_value, string_value, date_value);
+ALTER TABLE AttrVal_ds2324_HshldObsrvtn
+   ADD FOREIGN KEY (attribute_stable_id) REFERENCES AttributeGraph (stable_id);
+CREATE INDEX AttrVal_ds2324_HshldObsrvtn_i1
+ON AttrVal_ds2324_HshldObsrvtn (attribute_stable_id, HshldObsrvtn_id, number_value, string_value, date_value);
 -- for test db only
-CREATE unique INDEX AttrVal_1000_HshldObsrvtn_i2 ON AttrVal_1000_HshldObsrvtn (ontology_term_id, HshldObsrvtn_id);
+CREATE unique INDEX AttrVal_ds2324_HshldObsrvtn_i2 ON AttrVal_ds2324_HshldObsrvtn (attribute_stable_id, HshldObsrvtn_id);
 
-create table Ancestors_1000_HshldObsrvtn (
+create table Ancestors_ds2324_HshldObsrvtn (
   HshldObsrvtn_id integer,
   Hshld_id integer,
   PRIMARY KEY (Hshld_id)
-
 );
 
-create table AttrVal_1000_Prtcpnt (
+create table AttrVal_ds2324_Prtcpnt (
   prtcpnt_id integer,
-  ontology_term_id integer,
+  attribute_stable_id varchar(30),
   number_value integer,
   string_value varchar(100),
   date_value varchar(30),
 );
-ALTER TABLE AttrVal_1000_Prtcpnt
-   ADD FOREIGN KEY (ontology_term_id) REFERENCES AttributeGraph (ontology_term_id);
-CREATE INDEX AttrVal_1000_Prtcpnt_i1
-ON AttrVal_1000_Prtcpnt (ontology_term_id, prtcpnt_id, number_value, string_value, date_value);
+ALTER TABLE AttrVal_ds2324_Prtcpnt
+   ADD FOREIGN KEY (attribute_stable_id) REFERENCES AttributeGraph (stable_id);
+CREATE INDEX AttrVal_ds2324_Prtcpnt_i1
+ON AttrVal_ds2324_Prtcpnt (attribute_stable_id, prtcpnt_id, number_value, string_value, date_value);
 -- for test db only
-CREATE unique INDEX AttrVal_1000_Prtcpnt_i2 ON AttrVal_1000_Prtcpnt (ontology_term_id, prtcpnt_id);
+CREATE unique INDEX AttrVal_ds2324_Prtcpnt_i2 ON AttrVal_ds2324_Prtcpnt (attribute_stable_id, prtcpnt_id);
 
-create table Ancestors_1000_Prtcpnt (
+create table Ancestors_ds2324_Prtcpnt (
   prtcpnt_id integer,
   hshld_id integer,
   PRIMARY KEY (prtcpnt_id)
 );
 
-create table AttrVal_1000_PrtcpntObsrvtn (
+create table AttrVal_ds2324_PrtcpntObsrvtn (
   PrtcpntObsrvtn_id integer,
-  ontology_term_id integer,
+  attribute_stable_id varchar(30),
   number_value integer, 
   string_value varchar(100),
   date_value varchar(30),
 );
-ALTER TABLE AttrVal_1000_PrtcpntObsrvtn
-   ADD FOREIGN KEY (ontology_term_id) REFERENCES AttributeGraph (ontology_term_id);
-CREATE INDEX AttrVal_1000_PrtcpntObsrvtn_i1
-ON AttrVal_1000_PrtcpntObsrvtn (ontology_term_id, PrtcpntObsrvtn_id, number_value, string_value, date_value);
+ALTER TABLE AttrVal_ds2324_PrtcpntObsrvtn
+   ADD FOREIGN KEY (attribute_stable_id) REFERENCES AttributeGraph (stable_id);
+CREATE INDEX AttrVal_ds2324_PrtcpntObsrvtn_i1
+ON AttrVal_ds2324_PrtcpntObsrvtn (attribute_stable_id, PrtcpntObsrvtn_id, number_value, string_value, date_value);
 -- for test db only
-CREATE unique INDEX AttrVal_1000_PrtcpntObsrvtn_i2 ON AttrVal_1000_PrtcpntObsrvtn (ontology_term_id, PrtcpntObsrvtn_id);
+CREATE unique INDEX AttrVal_ds2324_PrtcpntObsrvtn_i2 ON AttrVal_ds2324_PrtcpntObsrvtn (attribute_stable_id, PrtcpntObsrvtn_id);
 
-create table Ancestors_1000_PrtcpntObsrvtn (
+create table Ancestors_ds2324_PrtcpntObsrvtn (
   PrtcpntObsrvtn_id integer,
   Prtcpnt_id integer,
   Hshld_id integer,

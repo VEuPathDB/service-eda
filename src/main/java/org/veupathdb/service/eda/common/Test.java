c4baa18ebead4fc55ea1a651e412d1907e4c95e8
Ryan Doherty <rdoherty@upenn.edu>
2021-02-11 02:48:28 -0500

    move more things around

--- src/main/java/org/veupathdb/service/eda/common/Test.java
+++ src/main/java/org/veupathdb/service/eda/common/Test.java
@@ -1,15 +1,23 @@
 package org.veupathdb.service.eda.common;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.gusdb.fgputil.FormatUtil;
 import org.veupathdb.service.eda.generated.model.VariableSpec;
 import org.veupathdb.service.eda.generated.model.VariableSpecImpl;
 
 public class Test {
 
-  public static void main(String[] args) throws Exception {
-    VariableSpec var = new VariableSpecImpl();
-    var.setEntityId("myEntity");
-    var.setVariableId("myVariable");
-    System.out.println(new ObjectMapper().writerFor(VariableSpec.class).writeValueAsString(var));
+  public static String getSampleVariableJsonStringSize() {
+    try {
+      VariableSpec var = new VariableSpecImpl();
+      var.setEntityId("myEntity");
+      var.setVariableId("myVariable");
+      String s = new ObjectMapper().writerFor(VariableSpec.class).writeValueAsString(var);
+      return s + " has " + FormatUtil.getUtf8EncodedBytes(s).length + " UTF-8 encoded bytes!";
+    }
+    catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
   }
 }

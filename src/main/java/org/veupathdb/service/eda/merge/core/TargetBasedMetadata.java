a13430805b87fc8d71848aa3a322110cdff1e329
Ryan Doherty <rdoherty@upenn.edu>
2021-03-10 15:18:34 -0500

    upgrade core lib

--- src/main/java/org/veupathdb/service/eda/merge/core/TargetBasedMetadata.java
+++ src/main/java/org/veupathdb/service/eda/merge/core/TargetBasedMetadata.java
@@ -1,4 +1,11 @@
 package org.veupathdb.service.eda.ms.core;
 
+import org.veupathdb.service.eda.common.model.EntityDef;
+import org.veupathdb.service.eda.common.model.ReferenceMetadata;
+
 public class TargetBasedMetadata {
+
+  public TargetBasedMetadata(ReferenceMetadata metadata, EntityDef targetEntity) {
+
+  }
 }

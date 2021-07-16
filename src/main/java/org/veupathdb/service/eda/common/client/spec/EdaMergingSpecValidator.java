0ecbc6a8b2d9cfb51784cefc1001bca1f841abde
Ryan Doherty <rdoherty@upenn.edu>
2021-07-15 23:03:48 -0400

    Add merging service request validation

--- src/main/java/org/veupathdb/service/eda/common/client/spec/EdaMergingSpecValidator.java
+++ src/main/java/org/veupathdb/service/eda/common/client/spec/EdaMergingSpecValidator.java
@@ -2,11 +2,14 @@ package org.veupathdb.service.eda.common.client.spec;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.gusdb.fgputil.json.JsonUtil;
 import org.gusdb.fgputil.validation.ValidationBundle;
 import org.gusdb.fgputil.validation.ValidationLevel;
 import org.veupathdb.service.eda.common.model.EntityDef;
 import org.veupathdb.service.eda.common.model.ReferenceMetadata;
+import org.veupathdb.service.eda.common.model.VariableDef;
 import org.veupathdb.service.eda.generated.model.VariableSpec;
 
 public class EdaMergingSpecValidator implements StreamSpecValidator {
@@ -15,8 +18,37 @@ public class EdaMergingSpecValidator implements StreamSpecValidator {
   public ValidationBundle validateStreamSpecs(Collection<StreamSpec> streamSpecs, ReferenceMetadata metadata) {
     ValidationBundle.ValidationBundleBuilder validation = ValidationBundle.builder(ValidationLevel.RUNNABLE);
     checkUniqueNames(streamSpecs, validation);
+    for (StreamSpec spec : streamSpecs) {
 
-    // TODO: validate merge service requests!
+      // validate requested entity ID
+      Optional<EntityDef> entityOpt = metadata.getEntity(spec.getEntityId());
+      if (entityOpt.isEmpty()) {
+        validation.addError(spec.getStreamName(), "No entity exists in study " +
+            metadata.getStudyId() + " with ID " + spec.getEntityId());
+        continue;
+      }
+      EntityDef requestedEntity = entityOpt.get();
+
+      // each requested variable must be a member of the entity or one of its ancestors
+      List<String> ancestorIds = metadata.getAncestors(requestedEntity)
+          .stream().map(EntityDef::getId).collect(Collectors.toList());
+      for (VariableSpec varSpec : spec) {
+        Optional<VariableDef> varOpt = metadata.getVariable(varSpec);
+        if (varOpt.isEmpty()) {
+          validation.addError(spec.getStreamName(), "No variable exists in study " +
+              metadata.getStudyId() + " with spec " + VariableDef.toDotNotation(varSpec));
+        }
+        // variable exists; make sure its entity is the requested entity or an ancestor
+        String varEntityId = varOpt.get().getEntityId();
+        if (!varEntityId.equals(requestedEntity.getId()) && !ancestorIds.contains(varEntityId)) {
+          validation.addError(spec.getStreamName(), "Requested variable '" + VariableDef.toDotNotation(varOpt.get()) +
+              "' must be a member of the requested entity '" + requestedEntity.getId() +
+              "' or one of its ancestors: [" + ancestorIds.stream().collect(Collectors.joining(", ")) + "]");
+        }
+      }
+    }
+
+    // TODO: validate merge service requests MORE?
 
     return validation.build();
   }

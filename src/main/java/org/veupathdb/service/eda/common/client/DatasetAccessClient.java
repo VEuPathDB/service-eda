7244f662d288669d2b24d6362aba7db649395a4a
Ryan Doherty <rdoherty@upenn.edu>
2021-12-16 14:07:24 -0500

    Add option to avoid dataset access restrictions in EDA backend

--- src/main/java/org/veupathdb/service/eda/common/client/DatasetAccessClient.java
+++ src/main/java/org/veupathdb/service/eda/common/client/DatasetAccessClient.java
@@ -5,16 +5,22 @@ import java.util.Map.Entry;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.MediaType;
 import org.gusdb.fgputil.client.ClientUtil;
+import org.gusdb.fgputil.runtime.Environment;
 import org.json.JSONObject;
 import org.veupathdb.service.eda.common.auth.StudyAccess;
 
 public class DatasetAccessClient extends ServiceClient {
+
+  private static final String ENABLE_DATASET_ACCESS_RESTRICTIONS = "ENABLE_DATASET_ACCESS_RESTRICTIONS";
 
   public DatasetAccessClient(String baseUrl, Entry<String,String> authHeader) {
     super(baseUrl, authHeader);
   }
 
   public StudyAccess getStudyAccess(String studyId) {
+    if (!Boolean.parseBoolean(Environment.getOptionalVar(ENABLE_DATASET_ACCESS_RESTRICTIONS, Boolean.TRUE.toString()))) {
+      return new StudyAccess(true, true, true, true, true);
+    }
     try (InputStream response = ClientUtil.makeAsyncGetRequest(getUrl("/permissions"),
         MediaType.APPLICATION_JSON, getAuthHeaderMap()).getInputStream()) {
       String permissionsJson = ClientUtil.readSmallResponseBody(response);

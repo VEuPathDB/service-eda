691b07ad062d83fede92f34f941f26f517030773
Ryan Doherty <tech@conical.org>
2022-03-17 00:00:16 -0400

    Upgrade to latest core and EdaCommon

--- src/main/java/org/veupathdb/service/eda/merge/Service.java
+++ src/main/java/org/veupathdb/service/eda/merge/Service.java
@@ -1,16 +1,18 @@
 package org.veupathdb.service.eda.ms;
 
 import java.util.Map.Entry;
+import java.util.Objects;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.glassfish.jersey.server.ContainerRequest;
 import org.gusdb.fgputil.validation.ValidationException;
+import org.veupathdb.lib.container.jaxrs.providers.UserProvider;
 import org.veupathdb.lib.container.jaxrs.server.annotations.Authenticated;
 import org.veupathdb.lib.container.jaxrs.server.annotations.DisableJackson;
-import org.veupathdb.lib.container.jaxrs.utils.RequestKeys;
-import org.veupathdb.service.eda.common.auth.StudyAccess;
 import org.veupathdb.service.eda.generated.model.EntityTabularPostResponseStream;
 import org.veupathdb.service.eda.generated.model.MergedEntityTabularPostRequest;
 import org.veupathdb.service.eda.generated.resources.Query;
@@ -22,14 +24,15 @@ public class Service implements Query {
   private static final Logger LOG = LogManager.getLogger(Service.class);
 
   @Context
-  private ContainerRequestContext _request;
+  ContainerRequest _request;
 
   @DisableJackson
   @Override
   public PostQueryResponse postQuery(MergedEntityTabularPostRequest requestBody) {
     try {
       // no need to check perms; only internal clients can access this endpoint
-      Entry<String,String> authHeader = StudyAccess.readAuthHeader(_request, RequestKeys.AUTH_HEADER);
+      Entry<String,String> authHeader = UserProvider
+          .getSubmittedAuth(_request).orElseThrow();
       return PostQueryResponse.respond200WithTextTabSeparatedValues(
           new EntityTabularPostResponseStream(
               new MergeRequestProcessor(requestBody, authHeader)

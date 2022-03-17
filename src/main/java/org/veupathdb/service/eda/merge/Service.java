f11158b533aa69146364bd855880b21729eb878f
Ryan Doherty <tech@conical.org>
2022-03-17 00:05:41 -0400

    improve exception and its message

--- src/main/java/org/veupathdb/service/eda/merge/Service.java
+++ src/main/java/org/veupathdb/service/eda/merge/Service.java
@@ -1,11 +1,8 @@
 package org.veupathdb.service.eda.ms;
 
 import java.util.Map.Entry;
-import java.util.Objects;
 import javax.ws.rs.BadRequestException;
-import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Request;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.glassfish.jersey.server.ContainerRequest;
@@ -13,6 +10,7 @@ import org.gusdb.fgputil.validation.ValidationException;
 import org.veupathdb.lib.container.jaxrs.providers.UserProvider;
 import org.veupathdb.lib.container.jaxrs.server.annotations.Authenticated;
 import org.veupathdb.lib.container.jaxrs.server.annotations.DisableJackson;
+import org.veupathdb.lib.container.jaxrs.utils.RequestKeys;
 import org.veupathdb.service.eda.generated.model.EntityTabularPostResponseStream;
 import org.veupathdb.service.eda.generated.model.MergedEntityTabularPostRequest;
 import org.veupathdb.service.eda.generated.resources.Query;
@@ -23,6 +21,10 @@ public class Service implements Query {
 
   private static final Logger LOG = LogManager.getLogger(Service.class);
 
+  private static final String MISSING_AUTH_MSG =
+      "Request must include authentication information in the form of a " +
+      RequestKeys.AUTH_HEADER + " header or query param";
+
   @Context
   ContainerRequest _request;
 
@@ -32,7 +34,7 @@ public class Service implements Query {
     try {
       // no need to check perms; only internal clients can access this endpoint
       Entry<String,String> authHeader = UserProvider
-          .getSubmittedAuth(_request).orElseThrow();
+          .getSubmittedAuth(_request).orElseThrow(() -> new BadRequestException(MISSING_AUTH_MSG));
       return PostQueryResponse.respond200WithTextTabSeparatedValues(
           new EntityTabularPostResponseStream(
               new MergeRequestProcessor(requestBody, authHeader)

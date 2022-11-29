ecf183a86170982e7e42c4cef9bf3c4e383360d2
Ryan Doherty <tech@conical.org>
2022-11-29 01:35:38 -0500

    Upgrade to edacommon to insert computed vars into reference metadata; comply with fallout

--- src/main/java/org/veupathdb/service/eda/merge/core/MergeRequestProcessor.java
+++ src/main/java/org/veupathdb/service/eda/merge/core/MergeRequestProcessor.java
@@ -5,11 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -19,6 +16,7 @@ import jakarta.ws.rs.BadRequestException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.gusdb.fgputil.ListBuilder;
+import org.gusdb.fgputil.Tuples.TwoTuple;
 import org.gusdb.fgputil.client.ResponseFuture;
 import org.gusdb.fgputil.functional.FunctionalInterfaces.ConsumerWithException;
 import org.gusdb.fgputil.iterator.IteratorUtil;
@@ -51,7 +49,7 @@ public class MergeRequestProcessor {
   private final String _targetEntityId;
   private final List<DerivedVariableSpec> _derivedVariables;
   private final List<VariableSpec> _outputVarSpecs;
-  private final ComputeSpecForMerging _computeRequestSpec;
+  private final Optional<TwoTuple<String,ComputeRequestBody>> _computeInfo;
   private final Entry<String, String> _authHeader;
 
   public MergeRequestProcessor(MergedEntityTabularPostRequest request, Entry<String, String> authHeader) {
@@ -61,24 +59,38 @@ public class MergeRequestProcessor {
     _targetEntityId = request.getEntityId();
     _derivedVariables = request.getDerivedVariables();
     _outputVarSpecs = request.getOutputVariables();
-    _computeRequestSpec = request.getComputeSpec();
     _authHeader = authHeader;
+    _computeInfo = Optional.ofNullable(request.getComputeSpec()).map(spec -> new TwoTuple<>(spec.getComputeName(),
+        new ComputeRequestBody(_studyId, _filters, _derivedVariables, spec.getComputeConfig())));
+
   }
 
   public Consumer<OutputStream> createMergedResponseSupplier() throws ValidationException {
 
-    // create subsetting client
+    // create subsetting and compute clients
     EdaSubsettingClient subsetSvc = new EdaSubsettingClient(Resources.SUBSETTING_SERVICE_URL, _authHeader);
     EdaComputeClient computeSvc = new EdaComputeClient(Resources.COMPUTE_SERVICE_URL, _authHeader);
 
     // build metadata for requested study
     APIStudyDetail studyDetail = subsetSvc.getStudy(_studyId)
         .orElseThrow(() -> new ValidationException("No study found with ID " + _studyId));
-    ReferenceMetadata metadata = new ReferenceMetadata(studyDetail, _derivedVariables);
+
+    // if compute specified, check if compute results are available
+    if (_computeInfo.isPresent() && !computeSvc.isJobResultsAvailable(_computeInfo.get().getFirst(), _computeInfo.get().getSecond())) {
+      throw new BadRequestException("Compute results are not available for the requested job.");
+    }
+
+    // if compute specified, get computed var metadata
+    List<VariableMapping> computedVars = _computeInfo
+        .map(info -> computeSvc.getJobVariableMetadata(info.getFirst(), info.getSecond()).getVariables())
+        .orElse(Collections.emptyList());
+
+    // create reference metadata using collected information
+    ReferenceMetadata metadata = new ReferenceMetadata(studyDetail, computedVars, _derivedVariables);
 
     // validation of incoming request
     //  (validation based specifically on the requested entity done during spec creation)
-    validateIncomingRequest(_targetEntityId, _outputVarSpecs, metadata, _computeRequestSpec);
+    validateIncomingRequest(_targetEntityId, _outputVarSpecs, metadata, _computeInfo);
 
     // request validated; convert requested entity and vars to defs
     EntityDef targetEntity = metadata.getEntity(_targetEntityId).orElseThrow();
@@ -86,18 +98,18 @@ public class MergeRequestProcessor {
     List<VariableSpec> outputVars = new ArrayList<>(outputVarDefs.stream().map(v -> (VariableSpec)v).toList());
 
     // build specs for streams to be merged into this request's response
-    Optional<EntityDef> computedEntity = Optional.ofNullable(_computeRequestSpec)
-        .map(spec -> metadata.getEntity(spec.getComputeConfig().getOutputEntityId()).orElseThrow());
+    Optional<EntityDef> computedEntity = _computeInfo
+        .map(info -> metadata.getEntity(info.getSecond().getConfig().getOutputEntityId()).orElseThrow());
     Map<String, StreamSpec> requiredStreams = new SubsettingStreamSpecFactory(metadata, targetEntity, computedEntity, outputVarDefs).createSpecs();
 
-    // add stream spec for compute if compute config appended on request TODO: validate against target entity!
-    ComputeRequestBody computeRequestBody = addComputedDataSpecs(metadata, computeSvc, requiredStreams, outputVars);
+    // if computed vars present, add stream spec for compute and add computed vars to output columns
+    addComputedDataSpecs(computedVars, requiredStreams, outputVars);
 
     // create stream generator
     Function<StreamSpec, ResponseFuture> streamGenerator = spec ->
         COMPUTED_VAR_STREAM_NAME.equals(spec.getStreamName())
         // need to get compute stream from compute service
-        ? computeSvc.getJobTabularOutput(_computeRequestSpec.getComputeName(), computeRequestBody)
+        ? computeSvc.getJobTabularOutput(_computeInfo.get().getFirst(), _computeInfo.get().getSecond())
         // all other streams come from subsetting service
         : subsetSvc.getTabularDataStream(metadata, _filters, Optional.empty(), spec);
 
@@ -112,44 +124,31 @@ public class MergeRequestProcessor {
     };
   }
 
-  private ComputeRequestBody addComputedDataSpecs(
-      ReferenceMetadata metadata,
-      EdaComputeClient computeSvc,
+  private void addComputedDataSpecs(
+      List<VariableMapping> varMappings,
       Map<String, StreamSpec> requiredStreams,
       List<VariableSpec> outputVars) {
 
-    // if no compute spec sent, don't need to add stream and can return null here
-    if (_computeRequestSpec == null) return null;
-
-    // create a request body from parts of the merge service request
-    ComputeRequestBody computeRequestBody = new ComputeRequestBody(metadata.getStudyId(),
-        _filters, _derivedVariables, _computeRequestSpec.getComputeConfig());
-
-    // check for results availability first
-    if (!computeSvc.isJobResultsAvailable(_computeRequestSpec.getComputeName(), computeRequestBody))
-      throw new BadRequestException("Results not ready for specified compute job.");
+    // if no computed vars present, nothing to do
+    if (varMappings.isEmpty()) return;
 
     // create variable specs from computed var metadata
-    List<VariableSpec> computedVars = computeSvc
-        .getJobVariableMetadata(_computeRequestSpec.getComputeName(), computeRequestBody)
-        .getVariables().stream()
+    List<VariableSpec> computedVars = varMappings.stream()
         .map(VariableMapping::getVariableSpec)
         .collect(Collectors.toList());
 
     // use computed var specs to create a stream spec and add them to output vars
     requiredStreams.put(COMPUTED_VAR_STREAM_NAME, new StreamSpec(COMPUTED_VAR_STREAM_NAME,
-        _computeRequestSpec.getComputeConfig().getOutputEntityId()).addVars(computedVars));
+        varMappings.get(0).getVariableSpec().getEntityId()).addVars(computedVars));
     outputVars.addAll(computedVars);
 
-    // return compute request for use in tabular request
-    return computeRequestBody;
   }
 
   private static void validateIncomingRequest(
       String targetEntityId,
       List<VariableSpec> outputVars,
       ReferenceMetadata metadata,
-      ComputeSpecForMerging computeRequestSpec) throws ValidationException {
+      Optional<TwoTuple<String,ComputeRequestBody>> computeInfo) throws ValidationException {
     StreamSpec requestSpec = new StreamSpec("incoming", targetEntityId);
     requestSpec.addAll(outputVars);
     new EdaMergingSpecValidator()
@@ -157,10 +156,10 @@ public class MergeRequestProcessor {
       .throwIfInvalid();
 
     // no need to check compute if it doesn't exist
-    if (computeRequestSpec == null) return;
+    if (computeInfo.isEmpty()) return;
 
-    // compute present; make sure computed var entity is the same as, or an ancestor of the target entity (needed for now)
-    Predicate<String> isComputeVarEntity = entityId -> entityId.equals(computeRequestSpec.getComputeConfig().getOutputEntityId());
+    // compute present; make sure computed var entity is the same as, or an ancestor of, the target entity (needed for now)
+    Predicate<String> isComputeVarEntity = entityId -> entityId.equals(computeInfo.get().getSecond().getConfig().getOutputEntityId());
     if (!isComputeVarEntity.test(targetEntityId) && metadata
         .getAncestors(metadata.getEntity(targetEntityId).orElseThrow()).stream()
         .filter(entity -> isComputeVarEntity.test(entity.getId()))

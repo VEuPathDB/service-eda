960d55c62a5275b791d662575cfd78dc1976fe00
Ryan Doherty <tech@conical.org>
2023-03-15 10:46:30 -0400

    Create a public merging endpoint that checks dataset access

--- src/main/java/org/veupathdb/service/eda/merge/Service.java
+++ src/main/java/org/veupathdb/service/eda/merge/Service.java
@@ -11,13 +11,15 @@ import org.veupathdb.lib.container.jaxrs.providers.UserProvider;
 import org.veupathdb.lib.container.jaxrs.server.annotations.Authenticated;
 import org.veupathdb.lib.container.jaxrs.server.annotations.DisableJackson;
 import org.veupathdb.lib.container.jaxrs.utils.RequestKeys;
+import org.veupathdb.service.eda.common.auth.StudyAccess;
 import org.veupathdb.service.eda.generated.model.EntityTabularPostResponseStream;
 import org.veupathdb.service.eda.generated.model.MergedEntityTabularPostRequest;
+import org.veupathdb.service.eda.generated.resources.MergingQueryExternal;
 import org.veupathdb.service.eda.generated.resources.Query;
 import org.veupathdb.service.eda.ms.core.MergeRequestProcessor;
 
 @Authenticated(allowGuests = true)
-public class Service implements Query {
+public class Service implements Query, MergingQueryExternal {
 
   private static final Logger LOG = LogManager.getLogger(Service.class);
 
@@ -31,14 +33,28 @@ public class Service implements Query {
   @DisableJackson
   @Override
   public PostQueryResponse postQuery(MergedEntityTabularPostRequest requestBody) {
+    // no need to check perms; only internal clients can access this endpoint
+    return PostQueryResponse.respond200WithTextTabSeparatedValues(processRequest(requestBody, false));
+  }
+
+  @DisableJackson
+  @Override
+  public PostMergingQueryExternalResponse postMergingQueryExternal(MergedEntityTabularPostRequest requestBody) {
+    // check access to full tabular results since this endpoint is intended to be exposed through traefik
+    return PostMergingQueryExternalResponse.respond200WithTextTabSeparatedValues(processRequest(requestBody, true));
+  }
+
+  private EntityTabularPostResponseStream processRequest(MergedEntityTabularPostRequest requestBody, boolean checkPermissions) {
     try {
-      // no need to check perms; only internal clients can access this endpoint
-      Entry<String,String> authHeader = UserProvider
-          .getSubmittedAuth(_request).orElseThrow(() -> new BadRequestException(MISSING_AUTH_MSG));
-      return PostQueryResponse.respond200WithTextTabSeparatedValues(
-          new EntityTabularPostResponseStream(
+      Entry<String,String> authHeader = UserProvider.getSubmittedAuth(_request)
+          .orElseThrow(() -> new BadRequestException(MISSING_AUTH_MSG));
+      if (checkPermissions) {
+        StudyAccess.confirmPermission(authHeader, Resources.DATASET_ACCESS_SERVICE_URL,
+            requestBody.getStudyId(), StudyAccess::allowResultsAll);
+      }
+      return new EntityTabularPostResponseStream(
               new MergeRequestProcessor(requestBody, authHeader)
-                  .createMergedResponseSupplier()));
+                  .createMergedResponseSupplier());
     }
     catch (ValidationException e) {
       LOG.error("Invalid request", e);

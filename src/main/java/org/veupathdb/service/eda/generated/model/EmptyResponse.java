451a70b45e745d3be1f7fd9b5a09040d2af47e43
Dan Galdi <dmgaldi@upenn.edu>
2024-04-10 09:54:20 -0400

    Remove generated code

--- src/main/java/org/veupathdb/service/eda/generated/model/EmptyResponse.java
+++
@@ -1,9 +0,0 @@
-package org.veupathdb.service.eda.generated.model;
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-@JsonDeserialize(
-    as = EmptyResponseImpl.class
-)
-public interface EmptyResponse {
-}

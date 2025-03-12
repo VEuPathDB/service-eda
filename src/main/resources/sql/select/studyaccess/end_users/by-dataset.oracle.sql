fe6bf8ac764cf09a7fe06311fefaba0ca9911628
Steve <43149795+steve-fischer-200@users.noreply.github.com>
2025-03-12 15:58:16 -0400

    Pg support (#82)
    * add pg specific files
    
    * add pg select queries
    
    * fix whitespace

--- src/main/resources/sql/select/studyaccess/end_users/by-dataset.oracle.sql
+++ src/main/resources/sql/select/studyaccess/end_users/by-dataset.oracle.sql
@@ -1,3 +1,4 @@
+-- This is the ORACLE version of this file.  See also the POSTGRES version
 SELECT
   v.*
 , (
@@ -34,7 +35,6 @@ FROM
     ON v.user_id = a.user_id
 WHERE
   v.dataset_presenter_id = ?
-  AND v.approval_status_id = ?
 ORDER BY
   user_id
 OFFSET ? ROWS

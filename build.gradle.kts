fe79b55f538cb46725f86d2d06e2fb42784eedbf
Ryan Doherty <tech@conical.org>
2025-03-18 18:18:35 -0400

    container core upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,7 +69,7 @@ repositories {
 //
 
 // versions
-val coreLib = "9.1.3"            // Container core lib version
+val coreLib = "9.4.4"            // Container core lib version
 val fgputil = "2.16.1-jakarta"   // FgpUtil version
 
 // use local EDA compute compiled schema if project exists, else use released version;

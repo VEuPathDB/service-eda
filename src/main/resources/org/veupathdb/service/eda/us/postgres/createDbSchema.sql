da6af3522936ab6555d149e900eba35d57550012
steve-fischer-200 <sfischer@upenn.edu>
2025-09-08 15:48:01 -0400

    add postgres userdb DDL

---
+++ src/main/resources/org/veupathdb/service/eda/us/postgres/createDbSchema.sql
@@ -0,0 +1,34 @@
+-- USERS table
+CREATE TABLE users (
+  user_id INTEGER NOT NULL,
+  is_guest INTEGER NOT NULL,
+  preferences TEXT, 
+  PRIMARY KEY (user_id)
+);
+
+-- Grant privileges (PostgreSQL syntax)
+GRANT SELECT, INSERT, UPDATE, DELETE ON users TO comm_wdk_w;
+
+-- ANALYSIS table
+CREATE TABLE analysis (
+  analysis_id VARCHAR(50) NOT NULL,
+  user_id INTEGER NOT NULL,
+  study_id VARCHAR(50) NOT NULL,
+  study_version VARCHAR(50),
+  api_version VARCHAR(50),
+  display_name VARCHAR(50) NOT NULL,
+  description TEXT,
+  creation_time TIMESTAMP NOT NULL,
+  modification_time TIMESTAMP NOT NULL,
+  is_public INTEGER NOT NULL,
+  num_filters INTEGER NOT NULL,
+  num_computations INTEGER NOT NULL,
+  num_visualizations INTEGER NOT NULL,
+  analysis_descriptor TEXT,
+  notes TEXT,
+  provenance TEXT,
+  PRIMARY KEY (analysis_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE INDEX analysis_user_id_idx ON analysis (user_id);

b0a0da7fe6ce3be5ff3373922b566a69033c6a9e
Ryan Doherty <385683+ryanrdoherty@users.noreply.github.com>
2026-05-11 12:16:25 -0400

    Support Temporary Tabular Results (#144)
    * Add endpoints to submit tabular result request, get an ID, and fetch that result later using a GET URL containing just the ID
    
    * Always reply with JSON media type regardless of what is asked for
    
    * Only support tabular format in temporary tabular results (not JSON)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 // adds repos for gradle plugin resolution and ensures github creds are provided to the build
 apply {
-  from("https://raw.githubusercontent.com/VEuPathDB/lib-gradle-container-utils/v4.8.9/includes/common.settings.gradle.kts")
+  from("https://raw.githubusercontent.com/VEuPathDB/lib-gradle-container-utils/v5.0.5/includes/common.settings.gradle.kts")
 }
